bbcbfee7594d0c2a6a95ddb26ba1b200b79f1986
Michael Register <mregister@smg.com>
2021-02-08 14:34:44 -0600

    Merged PR 1275: fix test

--- projects/smg360-core-services/src/lib/partial-translate-loader.service.ts
+++ projects/smg360-core-services/src/lib/partial-translate-loader.service.ts
@@ -11,11 +11,17 @@ export class PartialTranslateLoaderService {
   language:string;
   constructor(private http:HttpClient,private translate:TranslateService){
      this.language = localStorage.getItem('ls.languageIso') ?? translate.getBrowserCultureLang();
-     this.language = this.language.replace('"',"");
+     if(this.language){
+      this.language = this.language.replace('"',"");
+     }
+     
      this.translate.use(this.language);
   }
   
   addPart(url: string) {
+    if(this.language){
+      this.language = this.language.replace('"',"");
+     }
      return this.http.get(url+`?language=${this.language}`).pipe(map(results=>{  
       this.translate.setTranslation(this.language,results,true);
     }));
