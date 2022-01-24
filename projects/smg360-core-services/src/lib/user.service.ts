f2151d8af8f0f0ad526901b76d5c2280933d40d2
Sang Vo <svo@smg.com>
2022-01-24 08:04:59 -0600

    Merged PR 6107: G27-610 Only allow CI to view on 360 Admin
    G27-610 Only allow CI to view on 360 Admin

--- projects/smg360-core-services/src/lib/user.service.ts
+++ projects/smg360-core-services/src/lib/user.service.ts
@@ -1,31 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map,switchMap,take } from 'rxjs/operators';
+import { map, switchMap, take, tap } from 'rxjs/operators';
 import { AccountUtilityService } from './account-utility.service';
 import { CacheService } from './cache.service';
 import { CacheType } from './enums/cacheType.enum';
 import { EntityType } from './enums/entity-type.enum';
 import { PermissionService } from './permission.service';
-import { TranslateService } from '@ngx-translate/core'
+import { TranslateService } from '@ngx-translate/core';
 import { TranslateLoaderService } from './translate-loader.service';
+import { AppSettingsService } from './app-settings.service';
+import { LocalStorageService } from './local-storage.service';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   adminUserCacheKey = 'current-admin-user';
+  isAdmin = false;
+  isInternalUser = false;
 
-  constructor(private http: HttpClient,
+  constructor(
+    private http: HttpClient,
     private accountUtilityService: AccountUtilityService,
     private permissionService: PermissionService,
     private cacheService: CacheService,
     private translateLoaderService: TranslateLoaderService,
-    private translate: TranslateService) { }
+    private translate: TranslateService,
+    private appSettingsService: AppSettingsService,
+    private localStorageService: LocalStorageService) { }
 
+  initService() {
+    return this.getAdminUser().pipe(tap(user => {
+      this.isAdmin = user.isAdmin;
+      const auth: any = this.localStorageService.getObjectItem('authorizationData');
+      const groupIdCanAccessToAdmin = [this.appSettingsService.getSetting('groups').internalUser];
+      const jwtDecoded: any = jwt_decode(auth.token);
+      // get list group id from token claim, if the user have only one group the type of the group_id will be a string,
+      // otherwise it will be string array
+      const jwtGroup = jwtDecoded.group_id ? (typeof(jwtDecoded.group_id) === 'string' ? [jwtDecoded.group_id] : jwtDecoded.group_id) : [];
+      this.isInternalUser = jwtGroup.filter(g => groupIdCanAccessToAdmin.indexOf(g) >= 0).length > 0;
+    }));
+  }
 
   activate() {
-    const timezoneOffset = -(new Date("2015-01-01").getTimezoneOffset());
+    const timezoneOffset = -(new Date('2015-01-01').getTimezoneOffset());
   }
 
   getCachedUser() {
@@ -60,15 +80,16 @@ export class UserService {
             this.cacheService.set(CacheType.UserMeta, this.adminUserCacheKey, user);
             this.translateLoaderService.loadAdminTemplate();
             return user;
-          }))
+          }));
         }));
 
     } else {
       return new Observable((observer) => observer.next(currentUser));
     }
   }
+
   getCurrentUser() {
-    var currentUser = this.getCachedUser();
+    let currentUser = this.getCachedUser();
 
     if (!currentUser) {
       return this.getCurrent().pipe(take(1), switchMap(
@@ -81,17 +102,15 @@ export class UserService {
             if (defaultUserAccount) {
               this.accountUtilityService.setSelectedAccount(defaultUserAccount.account);
               return this.translateLoaderService.loadAccountTemplate(defaultUserAccount.account.id).pipe(map(results => {
-                var value = this.translate.instant('MENU');
-                console.log(value);
                 this.setCurrentUser(user);
                 return user;
               }));
             }
             else{
-              throw Error("No Accounts for user");
+              throw Error('No Accounts for user');
             }
           } else{
-            throw Error("No Accounts for user");
+            throw Error('No Accounts for user');
           }
         }));
 
@@ -101,13 +120,14 @@ export class UserService {
   }
 
   private getCurrent(): Observable<any> {
-    return this.http.get<any>("/api/user/current");
+    return this.http.get<any>('/api/user/current');
   }
+
   getGroupUsers(groupId: string | number): Observable<any> {
     return this.http.get<any>(`/api/user/groupUsers/${groupId}`);
   }
-  createUsers(usersCreateRequest: any): Observable<any> {
-    return this.http.post<any>("/api/user/create", usersCreateRequest);
-  }
 
+  createUsers(usersCreateRequest: any): Observable<any> {
+    return this.http.post<any>('/api/user/create', usersCreateRequest);
+  }
 }
