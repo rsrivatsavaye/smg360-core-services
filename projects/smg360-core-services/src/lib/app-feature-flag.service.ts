56d3fea7442f84587e5fbc9647a04dbb9eaacb76
Michael Ziluck <mziluck@smg.com>
2022-01-28 19:04:05 -0600

    Allow the constriction of the app feature flag type

--- projects/smg360-core-services/src/lib/app-feature-flag.service.ts
+++ projects/smg360-core-services/src/lib/app-feature-flag.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AppSettingsService } from './app-settings.service';
 
+/**
+ * Note about directive
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class AppFeatureFlagService {
+export class AppFeatureFlagService<T extends string = string> {
   private readonly FEATURE_NODE_NAME = 'features';
 
-  constructor(private appConfigService: AppSettingsService) {}
+  constructor(
+    private appConfigService: AppSettingsService
+  ) {
+  }
 
   /**
    * Indicated if a feature is enabled or not.
    * @param featureName - case sensitive feature name from environment.json file
    * @returns a boolean indicating if the feature is enabled or not.
    */
-  isFeatureEnabled(featureName: string): boolean {
+  isFeatureEnabled(featureName: T): boolean {
     return this.isTrue(
       this.appConfigService.getSetting(this.FEATURE_NODE_NAME)[featureName]
     );
