4224adb0c97580270533adcc1da08fea93e495c8
Michael Register <mregister@smg.com>
2022-03-14 14:16:27 -0500

    Merged PR 6862: update basecache service to use ngzone
    update basecache service to use ngzone

--- projects/smg360-core-services/src/lib/partial-translate-loader.service.ts
+++ projects/smg360-core-services/src/lib/partial-translate-loader.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, NgZone } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core'
+import { TranslateService } from '@ngx-translate/core';
 import { map } from 'rxjs/internal/operators/map';
 import { SessionCacheService } from './cache/session-cache.service';
 
@@ -8,33 +8,33 @@ import { SessionCacheService } from './cache/session-cache.service';
   providedIn: 'root'
 })
 export class PartialTranslateLoaderService {
-
-  language:string;
+  language: string;
   constructor(
-    private http:HttpClient,
-    private translate:TranslateService,
-    private sessionCacheService: SessionCacheService,
-    private ngZone: NgZone
-  ){
-     // TECH DEBT: This check is for backwards compatability, fix me once there are no `ls.` references in consuming apps.
-    this.language = localStorage.getItem('languageIso') ?? localStorage.getItem('ls.languageIso') ?? translate.getBrowserCultureLang();
+    private http: HttpClient,
+    private translate: TranslateService,
+    private sessionCacheService: SessionCacheService
+  ) {
+    // TECH DEBT: This check is for backwards compatability, fix me once there are no `ls.` references in consuming apps.
+    this.language =
+      localStorage.getItem('languageIso') ??
+      localStorage.getItem('ls.languageIso') ??
+      translate.getBrowserCultureLang();
 
-     if(this.language){
-      this.language = this.language.replace(/"/g,"");
-     }
+    if (this.language) {
+      this.language = this.language.replace(/"/g, '');
+    }
 
-     this.translate.use(this.language);
+    this.translate.use(this.language);
   }
 
   addPart(url: string) {
-    if(this.language){
-      this.language = this.language.replace(/"/g,"");
-     }
-     let extender = "?";
-     if(url.includes("?"))
-     {
-       extender = "&"
-     }
+    if (this.language) {
+      this.language = this.language.replace(/"/g, '');
+    }
+    let extender = '?';
+    if (url.includes('?')) {
+      extender = '&';
+    }
 
     const request$ = this.http.get(`${url + extender}language=${this.language}`);
 
@@ -43,12 +43,12 @@ export class PartialTranslateLoaderService {
       this.sessionCacheService,
       url,
       'translate-add-part-',
-      60000,
-      this.ngZone
-    ).pipe(map(results => {
-      this.translate.setTranslation(this.language, results, true);
-      return results;
-    }));
+      60000
+    ).pipe(
+      map(results => {
+        this.translate.setTranslation(this.language, results, true);
+        return results;
+      })
+    );
   }
-
 }
